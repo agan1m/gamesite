a9abd41e2f33015e6468828d61a52fc7c0c1331d
agan1m <marsov-v@mail.ru>
2017-11-29 23:43:13 +0300

    no message

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
-	// Custom JS
+	var box = document.querySelectorAll('.news__box__game');
+	console.log(box);
+    var event = function (e) {
+        console.log(e.style);
+
+
+    }
+    box,addEventListener('click', event);
 
 });
