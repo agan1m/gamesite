467aae3bde8a88f0eb42662e1c33c7f39b99e0e0
agan1m <marsov-v@mail.ru>
2017-12-01 14:47:24 +0300

    no message

--- app/js/common.js
+++ app/js/common.js
@@ -2,11 +2,24 @@ $(function() {
 
 	var box = document.querySelectorAll('.news__box__game');
 	console.log(box);
-    var event = function (e) {
-        console.log(e.style);
+	var mytooltip = function (e) {
+        var tooltip = e.target.querySelector('.game__tooltip');
+        console.dir(tooltip);
+        console.dir(tooltip.style.left);
+        var parent = tooltip.parentElement.parentElement;
+        console.dir(parent);
+        if (tooltip.style.left == 0+"px") {
+            tooltip.style.left = -999999 + "px";
+        }
+        else if (tooltip.style.left !== 0+"px") {
+            tooltip.style.left = 0;
+        }
+        /*tooltip.classList.toggle('game__tooltip_act');*/
 
 
+    };
+    for (var i=0; i<box.length; i++) {
+        box[i].addEventListener('click', mytooltip);
     }
-    box,addEventListener('click', event);
 
 });
